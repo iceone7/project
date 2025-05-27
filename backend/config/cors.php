6ca8f4e72b0c8dba524da4d93dbbd070384cec6e
Pavlik Amoiani <p.amoiani@gorgia.ge>
2025-05-27 12:56:04 +0400

    final result

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -20,7 +20,6 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => ['http://localhost:3000', 'http://localhost:3000', 'http://127.0.0.1:3000', 'http://127.0.0.1:8000', 'http://localhost:5173', 'http://localhost:5173/react/'],
-
     'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
