c99856a24a04196ea91b402ef6b549b819690c80
anri24 <anrivarshanidze2407@gmail.com>
2025-05-29 18:21:03 +0400

    changes

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -5,7 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return response()->json(['message' => 'Welcome to the Calls!']);
 })->withoutMiddleware('auth:sanctum');
-
-Route::get('/login', function () {
-    return response()->json(['status' => 'OK']);
-})->withoutMiddleware('auth:sanctum');
