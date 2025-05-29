6f418f40e4ebd1641f003d11544ab49279759fc2
Pavlik Amoiani <p.amoiani@gorgia.ge>
2025-05-29 17:57:25 +0400

    1

--- frontend/src/api/defaultInstance.js
+++ frontend/src/api/defaultInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const defaultInstance = axios.create({
   baseURL: `${import.meta.env.VITE_API_BASE_URL}`,
-  withCredentials: false,
+  withCredentials: true,
 });
 
 defaultInstance.interceptors.request.use(
