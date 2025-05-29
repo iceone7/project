c5eac209a62c30616a879ae7f5e51fff63ecad60
anri24 <anrivarshanidze2407@gmail.com>
2025-05-29 18:20:10 +0400

    chganges

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -6,3 +6,6 @@ Route::get('/', function () {
     return response()->json(['message' => 'Welcome to the Calls!']);
 })->withoutMiddleware('auth:sanctum');
 
+Route::get('/login', function () {
+    return response()->json(['status' => 'OK']);
+})->withoutMiddleware('auth:sanctum');
