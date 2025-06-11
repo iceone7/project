63ea48b38cdad74259e791546270836de255f7af
Pavlik Amoiani <p.amoiani@gorgia.ge>
2025-06-11 12:00:15 +0400

    fix defaultInstance

--- frontend/src/api/defaultInstance.js
+++ frontend/src/api/defaultInstance.js
@@ -1,7 +1,36 @@
+// import axios from 'axios';
+
+// const defaultInstance = axios.create({
+//   baseURL: `${import.meta.env.VITE_API_BASE_URL}`,
+//   headers: {
+//     Accept: "application/json",
+//     "Content-Type": "application/json",
+//     "X-Requested-With": "XMLHttpRequest",
+//   },
+//   withCredentials: true,
+//   xsrfCookieName: "XSRF-TOKEN",
+//   xsrfHeaderName: "X-XSRF-TOKEN",
+//   credentials: "include"
+// });
+
+// defaultInstance.interceptors.request.use(
+//   (config) => {
+//     const token = localStorage.getItem('authToken');
+//     if (token) {
+//       config.headers.Authorization = `Bearer ${token}`;
+//     }
+//     return config;
+//   },
+//   (error) => Promise.reject(error)
+// );
+
+// export default defaultInstance;
+
 import axios from 'axios';
 
 const defaultInstance = axios.create({
-  baseURL: `${import.meta.env.VITE_API_BASE_URL}`,
+  //baseURL: `${import.meta.env.REACT_APP_API_BASE_URL}`,
+  baseURL: `https://vip-back.gorgia.ge/api`,
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
@@ -25,3 +54,4 @@ defaultInstance.interceptors.request.use(
 );
 
 export default defaultInstance;
+
